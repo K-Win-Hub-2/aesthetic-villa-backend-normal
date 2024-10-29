53744e70150e431f0693656d893da97c06fb2c3a
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-29 10:48:29 +0630

    add: production db connection in db.config

--- config/db.js
+++ config/db.js
@@ -9,7 +9,9 @@ const config = {
       name: "Clinic-Denovo",
     },
     //db: 'mongodb://127.0.0.1:3221',
-    db: "mongodb+srv://pyaephyokwintech:5NhJPFwcnWwhBh97@cluster0.r1la8.mongodb.net/aesthetic-villa-test",
+    // db: "mongodb+srv://pyaephyokwintech:5NhJPFwcnWwhBh97@cluster0.r1la8.mongodb.net/aesthetic-villa-test",
+    //Production
+    db: "mongodb+srv://pyaephyokwintech:5NhJPFwcnWwhBh97@cluster0.r1la8.mongodb.net/Asethetic-Villa",
     //db: 'mongodb+srv://dbuser:P7qBNveg8bVO1d2z@cluster0.85ozwwv.mongodb.net/cherry-k?retryWrites=true&w=majority',
     uploadsURI: [
       "./uploads/cherry-k/img",
@@ -40,7 +42,9 @@ const config = {
     },
     //db: 'mongodb://127.0.0.1:3221',
     //db: 'mongodb+srv://dbuser:P7qBNveg8bVO1d2z@cluster0.85ozwwv.mongodb.net/cherry-k?retryWrites=true&w=majority',
-    db: "mongodb+srv://pyaephyokwintech:5NhJPFwcnWwhBh97@cluster0.r1la8.mongodb.net/aesthetic-villa-test",
+    // db: "mongodb+srv://pyaephyokwintech:5NhJPFwcnWwhBh97@cluster0.r1la8.mongodb.net/aesthetic-villa-test",
+    //Production
+    db: "mongodb+srv://pyaephyokwintech:5NhJPFwcnWwhBh97@cluster0.r1la8.mongodb.net/Asethetic-Villa",
     uploadsURI: [
       "./uploads/cherry-k/img",
       "./uploads/cherry-k/excel",
