d655299e8e3caf3efe9ff501c74401edae98d387
Kaung Set Hein <kaungsethein91@gmail.com>
2024-11-21 15:25:14 +0630

    fix

--- app/controllers/treatmentSelectionController.js
+++ app/controllers/treatmentSelectionController.js
@@ -321,8 +321,10 @@ exports.createMultiTreatmentSelection = async (req, res, next) => {
     const resultUpdate = await Patient.findOneAndUpdate(
       { _id: relatedPatient },
       {
-        totalAmount: parseInt(totalPaidAmount),
-        relatedMemberLevel: filterLevel[filterLevel.length - 1]?._id,
+        $inc: {
+          totalAmount: parseInt(totalPaidAmount),
+          relatedMemberLevel: filterLevel[filterLevel.length - 1]?._id,
+        },
       },
       { new: true }
     );
